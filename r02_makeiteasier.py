6e1ba6a240322e9c8ec956951683bdb580590d92
namgivu <namgivu@gmail.com>
2023-07-06 18:13:01 +0000

    make it easier 0th

---
+++ r02_makeiteasier.py
@@ -0,0 +1,372 @@
+def run():
+    TODO='''
+01/01 Capricorn    Ma Kết - Dê
+02/01 Capricorn    Ma Kết - Dê
+03/01 Capricorn    Ma Kết - Dê
+04/01 Capricorn    Ma Kết - Dê
+05/01 Capricorn    Ma Kết - Dê
+06/01 Capricorn    Ma Kết - Dê
+07/01 Capricorn    Ma Kết - Dê
+08/01 Capricorn    Ma Kết - Dê
+09/01 Capricorn    Ma Kết - Dê
+10/01 Capricorn    Ma Kết - Dê
+11/01 Capricorn    Ma Kết - Dê
+12/01 Capricorn    Ma Kết - Dê
+13/01 Capricorn    Ma Kết - Dê
+14/01 Capricorn    Ma Kết - Dê
+15/01 Capricorn    Ma Kết - Dê
+16/01 Capricorn    Ma Kết - Dê
+17/01 Capricorn    Ma Kết - Dê
+18/01 Capricorn    Ma Kết - Dê
+19/01 Capricorn    Ma Kết - Dê
+20/01 Aquarius     Bảo Bình - Người gánh nước
+21/01 Aquarius     Bảo Bình - Người gánh nước
+22/01 Aquarius     Bảo Bình - Người gánh nước
+23/01 Aquarius     Bảo Bình - Người gánh nước
+24/01 Aquarius     Bảo Bình - Người gánh nước
+25/01 Aquarius     Bảo Bình - Người gánh nước
+26/01 Aquarius     Bảo Bình - Người gánh nước
+27/01 Aquarius     Bảo Bình - Người gánh nước
+28/01 Aquarius     Bảo Bình - Người gánh nước
+29/01 Aquarius     Bảo Bình - Người gánh nước
+30/01 Aquarius     Bảo Bình - Người gánh nước
+31/01 Aquarius     Bảo Bình - Người gánh nước
+01/02 Aquarius     Bảo Bình - Người gánh nước
+02/02 Aquarius     Bảo Bình - Người gánh nước
+03/02 Aquarius     Bảo Bình - Người gánh nước
+04/02 Aquarius     Bảo Bình - Người gánh nước
+05/02 Aquarius     Bảo Bình - Người gánh nước
+06/02 Aquarius     Bảo Bình - Người gánh nước
+07/02 Aquarius     Bảo Bình - Người gánh nước
+08/02 Aquarius     Bảo Bình - Người gánh nước
+09/02 Aquarius     Bảo Bình - Người gánh nước
+10/02 Aquarius     Bảo Bình - Người gánh nước
+11/02 Aquarius     Bảo Bình - Người gánh nước
+12/02 Aquarius     Bảo Bình - Người gánh nước
+13/02 Aquarius     Bảo Bình - Người gánh nước
+14/02 Aquarius     Bảo Bình - Người gánh nước
+15/02 Aquarius     Bảo Bình - Người gánh nước
+16/02 Aquarius     Bảo Bình - Người gánh nước
+17/02 Aquarius     Bảo Bình - Người gánh nước
+18/02 Aquarius     Bảo Bình - Người gánh nước
+19/02 Pisces       Song Ngư - Cá
+20/02 Pisces       Song Ngư - Cá
+21/02 Pisces       Song Ngư - Cá
+22/02 Pisces       Song Ngư - Cá
+23/02 Pisces       Song Ngư - Cá
+24/02 Pisces       Song Ngư - Cá
+25/02 Pisces       Song Ngư - Cá
+26/02 Pisces       Song Ngư - Cá
+27/02 Pisces       Song Ngư - Cá
+28/02 Pisces       Song Ngư - Cá
+01/03 Pisces       Song Ngư - Cá
+02/03 Pisces       Song Ngư - Cá
+03/03 Pisces       Song Ngư - Cá
+04/03 Pisces       Song Ngư - Cá
+05/03 Pisces       Song Ngư - Cá
+06/03 Pisces       Song Ngư - Cá
+07/03 Pisces       Song Ngư - Cá
+08/03 Pisces       Song Ngư - Cá
+09/03 Pisces       Song Ngư - Cá
+10/03 Pisces       Song Ngư - Cá
+11/03 Pisces       Song Ngư - Cá
+12/03 Pisces       Song Ngư - Cá
+13/03 Pisces       Song Ngư - Cá
+14/03 Pisces       Song Ngư - Cá
+15/03 Pisces       Song Ngư - Cá
+16/03 Pisces       Song Ngư - Cá
+17/03 Pisces       Song Ngư - Cá
+18/03 Pisces       Song Ngư - Cá
+19/03 Pisces       Song Ngư - Cá
+20/03 Pisces       Song Ngư - Cá
+21/03 Aries        Bạch Dương - Cừu
+22/03 Aries        Bạch Dương - Cừu
+23/03 Aries        Bạch Dương - Cừu
+24/03 Aries        Bạch Dương - Cừu
+25/03 Aries        Bạch Dương - Cừu
+26/03 Aries        Bạch Dương - Cừu
+27/03 Aries        Bạch Dương - Cừu
+28/03 Aries        Bạch Dương - Cừu
+29/03 Aries        Bạch Dương - Cừu
+30/03 Aries        Bạch Dương - Cừu
+31/03 Aries        Bạch Dương - Cừu
+01/04 Aries        Bạch Dương - Cừu
+02/04 Aries        Bạch Dương - Cừu
+03/04 Aries        Bạch Dương - Cừu
+04/04 Aries        Bạch Dương - Cừu
+05/04 Aries        Bạch Dương - Cừu
+06/04 Aries        Bạch Dương - Cừu
+07/04 Aries        Bạch Dương - Cừu
+08/04 Aries        Bạch Dương - Cừu
+09/04 Aries        Bạch Dương - Cừu
+10/04 Aries        Bạch Dương - Cừu
+11/04 Aries        Bạch Dương - Cừu
+12/04 Aries        Bạch Dương - Cừu
+13/04 Aries        Bạch Dương - Cừu
+14/04 Aries        Bạch Dương - Cừu
+15/04 Aries        Bạch Dương - Cừu
+16/04 Aries        Bạch Dương - Cừu
+17/04 Aries        Bạch Dương - Cừu
+18/04 Aries        Bạch Dương - Cừu
+19/04 Aries        Bạch Dương - Cừu
+20/04 Taurus       Kim Ngưu - Bò mộng
+21/04 Taurus       Kim Ngưu - Bò mộng
+22/04 Taurus       Kim Ngưu - Bò mộng
+23/04 Taurus       Kim Ngưu - Bò mộng
+24/04 Taurus       Kim Ngưu - Bò mộng
+25/04 Taurus       Kim Ngưu - Bò mộng
+26/04 Taurus       Kim Ngưu - Bò mộng
+27/04 Taurus       Kim Ngưu - Bò mộng
+28/04 Taurus       Kim Ngưu - Bò mộng
+29/04 Taurus       Kim Ngưu - Bò mộng
+30/04 Taurus       Kim Ngưu - Bò mộng
+01/05 Taurus       Kim Ngưu - Bò mộng
+02/05 Taurus       Kim Ngưu - Bò mộng
+03/05 Taurus       Kim Ngưu - Bò mộng
+04/05 Taurus       Kim Ngưu - Bò mộng
+05/05 Taurus       Kim Ngưu - Bò mộng
+06/05 Taurus       Kim Ngưu - Bò mộng
+07/05 Taurus       Kim Ngưu - Bò mộng
+08/05 Taurus       Kim Ngưu - Bò mộng
+09/05 Taurus       Kim Ngưu - Bò mộng
+10/05 Taurus       Kim Ngưu - Bò mộng
+11/05 Taurus       Kim Ngưu - Bò mộng
+12/05 Taurus       Kim Ngưu - Bò mộng
+13/05 Taurus       Kim Ngưu - Bò mộng
+14/05 Taurus       Kim Ngưu - Bò mộng
+15/05 Taurus       Kim Ngưu - Bò mộng
+16/05 Taurus       Kim Ngưu - Bò mộng
+17/05 Taurus       Kim Ngưu - Bò mộng
+18/05 Taurus       Kim Ngưu - Bò mộng
+19/05 Taurus       Kim Ngưu - Bò mộng
+20/05 Taurus       Kim Ngưu - Bò mộng
+21/05 Gemini       Song Tử - Cặp song sinh
+22/05 Gemini       Song Tử - Cặp song sinh
+23/05 Gemini       Song Tử - Cặp song sinh
+24/05 Gemini       Song Tử - Cặp song sinh
+25/05 Gemini       Song Tử - Cặp song sinh
+26/05 Gemini       Song Tử - Cặp song sinh
+27/05 Gemini       Song Tử - Cặp song sinh
+28/05 Gemini       Song Tử - Cặp song sinh
+29/05 Gemini       Song Tử - Cặp song sinh
+30/05 Gemini       Song Tử - Cặp song sinh
+31/05 Gemini       Song Tử - Cặp song sinh
+01/06 Gemini       Song Tử - Cặp song sinh
+02/06 Gemini       Song Tử - Cặp song sinh
+03/06 Gemini       Song Tử - Cặp song sinh
+04/06 Gemini       Song Tử - Cặp song sinh
+05/06 Gemini       Song Tử - Cặp song sinh
+06/06 Gemini       Song Tử - Cặp song sinh
+07/06 Gemini       Song Tử - Cặp song sinh
+08/06 Gemini       Song Tử - Cặp song sinh
+09/06 Gemini       Song Tử - Cặp song sinh
+10/06 Gemini       Song Tử - Cặp song sinh
+11/06 Gemini       Song Tử - Cặp song sinh
+12/06 Gemini       Song Tử - Cặp song sinh
+13/06 Gemini       Song Tử - Cặp song sinh
+14/06 Gemini       Song Tử - Cặp song sinh
+15/06 Gemini       Song Tử - Cặp song sinh
+16/06 Gemini       Song Tử - Cặp song sinh
+17/06 Gemini       Song Tử - Cặp song sinh
+18/06 Gemini       Song Tử - Cặp song sinh
+19/06 Gemini       Song Tử - Cặp song sinh
+20/06 Gemini       Song Tử - Cặp song sinh
+21/06 Cancer       Cự Giải - Cua
+22/06 Cancer       Cự Giải - Cua
+23/06 Cancer       Cự Giải - Cua
+24/06 Cancer       Cự Giải - Cua
+25/06 Cancer       Cự Giải - Cua
+26/06 Cancer       Cự Giải - Cua
+27/06 Cancer       Cự Giải - Cua
+28/06 Cancer       Cự Giải - Cua
+29/06 Cancer       Cự Giải - Cua
+30/06 Cancer       Cự Giải - Cua
+01/07 Cancer       Cự Giải - Cua
+02/07 Cancer       Cự Giải - Cua
+03/07 Cancer       Cự Giải - Cua
+04/07 Cancer       Cự Giải - Cua
+05/07 Cancer       Cự Giải - Cua
+06/07 Cancer       Cự Giải - Cua
+07/07 Cancer       Cự Giải - Cua
+08/07 Cancer       Cự Giải - Cua
+09/07 Cancer       Cự Giải - Cua
+10/07 Cancer       Cự Giải - Cua
+11/07 Cancer       Cự Giải - Cua
+12/07 Cancer       Cự Giải - Cua
+13/07 Cancer       Cự Giải - Cua
+14/07 Cancer       Cự Giải - Cua
+15/07 Cancer       Cự Giải - Cua
+16/07 Cancer       Cự Giải - Cua
+17/07 Cancer       Cự Giải - Cua
+18/07 Cancer       Cự Giải - Cua
+19/07 Cancer       Cự Giải - Cua
+20/07 Cancer       Cự Giải - Cua
+21/07 Cancer       Cự Giải - Cua
+22/07 Cancer       Cự Giải - Cua
+23/07 Leo          Sư Tử - Sư tử
+24/07 Leo          Sư Tử - Sư tử
+25/07 Leo          Sư Tử - Sư tử
+26/07 Leo          Sư Tử - Sư tử
+27/07 Leo          Sư Tử - Sư tử
+28/07 Leo          Sư Tử - Sư tử
+29/07 Leo          Sư Tử - Sư tử
+30/07 Leo          Sư Tử - Sư tử
+31/07 Leo          Sư Tử - Sư tử
+01/08 Leo          Sư Tử - Sư tử
+02/08 Leo          Sư Tử - Sư tử
+03/08 Leo          Sư Tử - Sư tử
+04/08 Leo          Sư Tử - Sư tử
+05/08 Leo          Sư Tử - Sư tử
+06/08 Leo          Sư Tử - Sư tử
+07/08 Leo          Sư Tử - Sư tử
+08/08 Leo          Sư Tử - Sư tử
+09/08 Leo          Sư Tử - Sư tử
+10/08 Leo          Sư Tử - Sư tử
+11/08 Leo          Sư Tử - Sư tử
+12/08 Leo          Sư Tử - Sư tử
+13/08 Leo          Sư Tử - Sư tử
+14/08 Leo          Sư Tử - Sư tử
+15/08 Leo          Sư Tử - Sư tử
+16/08 Leo          Sư Tử - Sư tử
+17/08 Leo          Sư Tử - Sư tử
+18/08 Leo          Sư Tử - Sư tử
+19/08 Leo          Sư Tử - Sư tử
+20/08 Leo          Sư Tử - Sư tử
+21/08 Leo          Sư Tử - Sư tử
+22/08 Leo          Sư Tử - Sư tử
+23/08 Virgo        Xử Nữ - Trinh nữ
+24/08 Virgo        Xử Nữ - Trinh nữ
+25/08 Virgo        Xử Nữ - Trinh nữ
+26/08 Virgo        Xử Nữ - Trinh nữ
+27/08 Virgo        Xử Nữ - Trinh nữ
+28/08 Virgo        Xử Nữ - Trinh nữ
+29/08 Virgo        Xử Nữ - Trinh nữ
+30/08 Virgo        Xử Nữ - Trinh nữ
+31/08 Virgo        Xử Nữ - Trinh nữ
+01/09 Virgo        Xử Nữ - Trinh nữ
+02/09 Virgo        Xử Nữ - Trinh nữ
+03/09 Virgo        Xử Nữ - Trinh nữ
+04/09 Virgo        Xử Nữ - Trinh nữ
+05/09 Virgo        Xử Nữ - Trinh nữ
+06/09 Virgo        Xử Nữ - Trinh nữ
+07/09 Virgo        Xử Nữ - Trinh nữ
+08/09 Virgo        Xử Nữ - Trinh nữ
+09/09 Virgo        Xử Nữ - Trinh nữ
+10/09 Virgo        Xử Nữ - Trinh nữ
+11/09 Virgo        Xử Nữ - Trinh nữ
+12/09 Virgo        Xử Nữ - Trinh nữ
+13/09 Virgo        Xử Nữ - Trinh nữ
+14/09 Virgo        Xử Nữ - Trinh nữ
+15/09 Virgo        Xử Nữ - Trinh nữ
+16/09 Virgo        Xử Nữ - Trinh nữ
+17/09 Virgo        Xử Nữ - Trinh nữ
+18/09 Virgo        Xử Nữ - Trinh nữ
+19/09 Virgo        Xử Nữ - Trinh nữ
+20/09 Virgo        Xử Nữ - Trinh nữ
+21/09 Virgo        Xử Nữ - Trinh nữ
+22/09 Virgo        Xử Nữ - Trinh nữ
+23/09 Libra        Thiên Bình - Cân
+24/09 Libra        Thiên Bình - Cân
+25/09 Libra        Thiên Bình - Cân
+26/09 Libra        Thiên Bình - Cân
+27/09 Libra        Thiên Bình - Cân
+28/09 Libra        Thiên Bình - Cân
+29/09 Libra        Thiên Bình - Cân
+30/09 Libra        Thiên Bình - Cân
+01/10 Libra        Thiên Bình - Cân
+02/10 Libra        Thiên Bình - Cân
+03/10 Libra        Thiên Bình - Cân
+04/10 Libra        Thiên Bình - Cân
+05/10 Libra        Thiên Bình - Cân
+06/10 Libra        Thiên Bình - Cân
+07/10 Libra        Thiên Bình - Cân
+08/10 Libra        Thiên Bình - Cân
+09/10 Libra        Thiên Bình - Cân
+10/10 Libra        Thiên Bình - Cân
+11/10 Libra        Thiên Bình - Cân
+12/10 Libra        Thiên Bình - Cân
+13/10 Libra        Thiên Bình - Cân
+14/10 Libra        Thiên Bình - Cân
+15/10 Libra        Thiên Bình - Cân
+16/10 Libra        Thiên Bình - Cân
+17/10 Libra        Thiên Bình - Cân
+18/10 Libra        Thiên Bình - Cân
+19/10 Libra        Thiên Bình - Cân
+20/10 Libra        Thiên Bình - Cân
+21/10 Libra        Thiên Bình - Cân
+22/10 Libra        Thiên Bình - Cân
+23/10 Scorpio      Thiên Yết - Bọ cạp
+24/10 Scorpio      Thiên Yết - Bọ cạp
+25/10 Scorpio      Thiên Yết - Bọ cạp
+26/10 Scorpio      Thiên Yết - Bọ cạp
+27/10 Scorpio      Thiên Yết - Bọ cạp
+28/10 Scorpio      Thiên Yết - Bọ cạp
+29/10 Scorpio      Thiên Yết - Bọ cạp
+30/10 Scorpio      Thiên Yết - Bọ cạp
+31/10 Scorpio      Thiên Yết - Bọ cạp
+01/11 Scorpio      Thiên Yết - Bọ cạp
+02/11 Scorpio      Thiên Yết - Bọ cạp
+03/11 Scorpio      Thiên Yết - Bọ cạp
+04/11 Scorpio      Thiên Yết - Bọ cạp
+05/11 Scorpio      Thiên Yết - Bọ cạp
+06/11 Scorpio      Thiên Yết - Bọ cạp
+07/11 Scorpio      Thiên Yết - Bọ cạp
+08/11 Scorpio      Thiên Yết - Bọ cạp
+09/11 Scorpio      Thiên Yết - Bọ cạp
+10/11 Scorpio      Thiên Yết - Bọ cạp
+11/11 Scorpio      Thiên Yết - Bọ cạp
+12/11 Scorpio      Thiên Yết - Bọ cạp
+13/11 Scorpio      Thiên Yết - Bọ cạp
+14/11 Scorpio      Thiên Yết - Bọ cạp
+15/11 Scorpio      Thiên Yết - Bọ cạp
+16/11 Scorpio      Thiên Yết - Bọ cạp
+17/11 Scorpio      Thiên Yết - Bọ cạp
+18/11 Scorpio      Thiên Yết - Bọ cạp
+19/11 Scorpio      Thiên Yết - Bọ cạp
+20/11 Scorpio      Thiên Yết - Bọ cạp
+21/11 Scorpio      Thiên Yết - Bọ cạp
+22/11 Sagittarius  Nhân Mã - Cung thủ
+23/11 Sagittarius  Nhân Mã - Cung thủ
+24/11 Sagittarius  Nhân Mã - Cung thủ
+25/11 Sagittarius  Nhân Mã - Cung thủ
+26/11 Sagittarius  Nhân Mã - Cung thủ
+27/11 Sagittarius  Nhân Mã - Cung thủ
+28/11 Sagittarius  Nhân Mã - Cung thủ
+29/11 Sagittarius  Nhân Mã - Cung thủ
+30/11 Sagittarius  Nhân Mã - Cung thủ
+01/12 Sagittarius  Nhân Mã - Cung thủ
+02/12 Sagittarius  Nhân Mã - Cung thủ
+03/12 Sagittarius  Nhân Mã - Cung thủ
+04/12 Sagittarius  Nhân Mã - Cung thủ
+05/12 Sagittarius  Nhân Mã - Cung thủ
+06/12 Sagittarius  Nhân Mã - Cung thủ
+07/12 Sagittarius  Nhân Mã - Cung thủ
+08/12 Sagittarius  Nhân Mã - Cung thủ
+09/12 Sagittarius  Nhân Mã - Cung thủ
+10/12 Sagittarius  Nhân Mã - Cung thủ
+11/12 Sagittarius  Nhân Mã - Cung thủ
+12/12 Sagittarius  Nhân Mã - Cung thủ
+13/12 Sagittarius  Nhân Mã - Cung thủ
+14/12 Sagittarius  Nhân Mã - Cung thủ
+15/12 Sagittarius  Nhân Mã - Cung thủ
+16/12 Sagittarius  Nhân Mã - Cung thủ
+17/12 Sagittarius  Nhân Mã - Cung thủ
+18/12 Sagittarius  Nhân Mã - Cung thủ
+19/12 Sagittarius  Nhân Mã - Cung thủ
+20/12 Sagittarius  Nhân Mã - Cung thủ
+21/12 Sagittarius  Nhân Mã - Cung thủ
+22/12 Capricorn    Ma Kết - Dê
+23/12 Capricorn    Ma Kết - Dê
+24/12 Capricorn    Ma Kết - Dê
+25/12 Capricorn    Ma Kết - Dê
+26/12 Capricorn    Ma Kết - Dê
+27/12 Capricorn    Ma Kết - Dê
+28/12 Capricorn    Ma Kết - Dê
+29/12 Capricorn    Ma Kết - Dê
+30/12 Capricorn    Ma Kết - Dê
+31/12 Capricorn    Ma Kết - Dê
+'''    
+    
+    
+if __name__ == '__main__':
+    run()
